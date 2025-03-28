8877d2560e17bfcfc20ae45eeefc56e4c82523cb
Semi-Bicer <semih.bic.er@hotmail.com>
2025-03-28 18:43:49 +0300

    update

--- Hafta6/src/Main.java
+++ Hafta6/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
-        Vehicle a1 = new Vehicle(20243, "Mehmet Yılmaz");
-        Vehicle a2 = new Vehicle(20114, "İpek Şanlı");
+        Vehicle a1 = new Vehicle(20243, "Mehmet Yilmaz");
+        Vehicle a2 = new Vehicle(20114, "İpek Şanli");
         Vehicle a3 = new Vehicle(20243, "Ahmet Boz");
 
         Vehicle[] vehicles = {a1, a2, a3};
@@ -9,10 +9,10 @@ public class Main {
         ParkingLot o1 = new ParkingLot(vehicles, 7);
         o1.dislodgeVehicle(a1);
 
-        Vehicle a4 = new Vehicle(20765, "İsmail Yılmaz");
+        Vehicle a4 = new Vehicle(20765, "İsmail Yilmaz");
         o1.addVehicle(a4);
         o1.dislodgeVehicle(a2);
         System.out.println(o1);
-        System.out.println("Total balance: " );
+        System.out.println("Total balance: " + o1.getBalance());
     }
 }
